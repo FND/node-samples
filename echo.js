489279494922ae0f3b514271d886f7b776f9f9fc
FND <fnd@fnd-oda.(none)>
2011-03-20 10:09:53 +0100

    fixed whitespace and use of quotes
    tabs for indentation, double quotes by default

--- echo.js
+++ echo.js
@@ -1,8 +1,8 @@
-var net = require('net');
+var net = require("net");
 
 var server = net.createServer(function (socket) {
-  socket.write("Echo server\r\n");
-  socket.pipe(socket);
-})
+	socket.write("Echo server\r\n");
+	socket.pipe(socket);
+});
 
 server.listen(8124, "127.0.0.1");
